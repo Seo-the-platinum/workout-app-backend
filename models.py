9ac308c10f0db3cc76cad33769963b6bc810a240
Eliseo Flores <eliseo_flores530@yahoo.com>
2022-08-16 21:39:41 -0700

    changed postgres to postgresql for db path

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 database_path = (
     #"postgresql://postgres:seoisoe5i73@localhost/workoutdb"
-    "postgres://nrmkqlnllkiwnn:8fce5b5656fcf2611c7649946e26f279cfa830ce42afc6e035ef29a9e2585f07@ec2-44-205-112-253.compute-1.amazonaws.com:5432/daqg2chp8idec1"
+    "postgresql://nrmkqlnllkiwnn:8fce5b5656fcf2611c7649946e26f279cfa830ce42afc6e035ef29a9e2585f07@ec2-44-205-112-253.compute-1.amazonaws.com:5432/daqg2chp8idec1"
 )
 
 def setup_db(app, database_path=database_path):
